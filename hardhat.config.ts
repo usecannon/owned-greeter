bb592e77128c8bf25c3cf32f566b095355faf5cc
Matías <mjlescano@protonmail.com>
2023-03-03 15:56:04 +0100

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,15 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import 'dotenv'
+
 import 'hardhat-cannon'
 
 export default {
-  solidity: '0.8.19'
-}
+  solidity: '0.8.19',
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.GOERLI_PRIVATE_KEY ? [process.env.GOERLI_PRIVATE_KEY] : [],
+      chainId: 5,
+    },
+  }
+} satisfies HardhatUserConfig
