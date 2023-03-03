a49ee5f5e8a624ffe062f926c2b7c8890920093e
Matías <mjlescano@protonmail.com>
2023-03-03 17:37:41 +0100

    deploy contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,19 @@
+import 'dotenv/config'
 import { HardhatUserConfig } from 'hardhat/config'
-import 'dotenv'
 
 import 'hardhat-cannon'
 
 export default {
   solidity: '0.8.19',
+  defaultNetwork: 'cannon',
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: process.env.GOERLI_PRIVATE_KEY ? [process.env.GOERLI_PRIVATE_KEY] : [],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 5,
     },
+  },
+  cannon: {
+    publicSourceCode: true,
   }
 } satisfies HardhatUserConfig
